85f5f98d54a3bbbd0ece9f9ff700819ab356e029
QATomB <147164443+QATomB@users.noreply.github.com>
2023-12-12 13:47:06 +0000

    these are the final and tested versions
    these are the final and tested versions of all files

---
+++ source/biggestthreats.php
@@ -0,0 +1,491 @@
+<!DOCTYPE html>
+<html>
+	<link rel="stylesheet" href="colours.css">
+	<head>
+		<title>EcoWithYou - Biggest Threats</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+		<link rel="icon" type="image/x-icon" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAKUAAAClCAMAAAAK9c3oAAABoVBMVEVHcEym0pah
+z5H7/fqm0pb///+fzo7A37bU6c3U6cz8/fuezo2k0ZT+//6l0ZWezo2k0JSgzo+p
+05r+//6ezo6gzo+dzYyk0ZShz5D8/vz////+//6i0JL///7///////////+ezo2g
+z4/9/vz+//7///+ezo2gz5D///////////+q1JudzYzu9+v///+i0JKz2KbZ7NK8
+3bD////H47zY69HU6c3A37X///7F4bvG4ryv1qHD4bnH473L/MujzP94/4udzYx4
+xKX///+czYt3xKT+//77/vx2w6R3xKV3w6R9x6nl/Pd7xafl9O75/Prz+veHy6/q
+9vH3+/nv+PSByKuQz7W54dF5xaaEyq3i8uyp2sbQ6+Da7+eU0bi+49SLzbK0382j
+0JN/x6qu3Mmezo3e8enN6t7K6Nzx+O+32qqY0rql2MOx3cugz5CNzrOn0pjI477t
+9ums1Z3U7OLY69HT6cy83bGw16LO5sbo8+Si1sHD5df1+vPF5tnC4Lf7/fqf1b/Y
+7uWc1L3k8d/g8Nvc7taa07yd1L7W7eRHcEw8E0pqAAAAi3RSTlMAJIwpE/rqAwwH
+E/Q4pS7eHH1Der/S+m1XHPTDyE3Oimxlmj61XPyz2e6WTare4qWYyLHlpudR2urn
+b9d/9cf0////////////////////////////////////////////////////////
+//////////////////////////////////////////8AUq5CiQAAFKNJREFUeNrs
+WWtb2mgaHq1T2+pocWx1djq7q3PoztZeV9ud2dnZ2f2QhJDTlZDzgZAEhEI4iSJa
+ETyAWuRf7xtIOBOpPcx82PeLAsJ7c9/Pcz8HP/vs/+cPdu4/nFlc31gLzM5ubj3d
+2pwNBDbWV5Zm5ub/IPjm5xbXA5tPn/y2nz3bqxyCUzms7J1lj3Z37z5a3ZrdWJmZ
++90Rbmw+yf63cnBVygjh81AI6ZwQFhYypcuDyl52d2H5aWB95t7vBXFpY/O3Xytv
+S4KHbcwJCZmrytnRwvJq4NnDTw9xZuPVL5WrjA/APqgnx5U3+wuPZlc+pfbzD9df
+/HJYOh8kTXB+YFjI0VvgWwx/elpO8YL7OpY5ONtfWA0sfiKg84uzgxDDIlOuFZoX
+cdtW9GRBUQp6vUhpHK1SEtdUykIYa//deeYwu3v36cYnUP7+4otfL4UuQoEpW9Ui
+x8mmnpRUGvcOChMoDA5B4pLN8iLbCV6htLe/8GTtI+O89+ynn0tYV2TGynE0TpK4
+zqesHEXA4w4pp6Itlo8I7feFModHO48CMx8R4/qrg4wHkVf0JgfjbcbMllJUcXjC
+oYuUGVXslOiawcnb7M7y7NLHMfz5lVcHJ57SfDwH4x2IKJ3n2SQNTz4oIXEwzV1b
+rPvuk4Oj18uBj6H7zIufPR4R0YjGutTJpywSanEE7HfAqwTe7MXzyeHRD6vrH9rq
+59ZelrreGGlKRc29Xs3ziFjLx+AbDyFF+60rs7e7sLX4QWV/9uoS6zqjIZMo6YmZ
+A3ljqh3tbzgFbNDrj9+A8Pxwss/9++eT3qfzMtm9GJdaWNVxnimo1CLDNSl8uB9c
+XfxQEfnTcX8hZDjUi0E1pmBI+qKez9E3okRj4mjtLAE6N+5/iNR+9rJLJKh/PIsV
+KMm9+IIBIorxeFpXbyZTio4p+sLh7sLm+5vn3H+ed4shrxfEfF4QDVtqs6nazrMp
+TgWFBpybYGqWMNqIhC6PoCcr8++rdqb3kYYm83qiAX4BKEkUl9qRhkVydELjOI7y
+NyOYlqxxLVTmbOfu2vuoPj/zsi9tEKzRwgxLRFImDFM5uWm7SZuyWxGeFe2YL51k
+LhoZ34JWthcC72Gdiy/DQx8ZreqFFKMUrDQjssP3NfxhSmMU73z7g92F2Vt3dCvP
+BxUKp9hyEaVrIGPsqqxgI9elfUUn6PgEmMjV/uPNW8L85wBIwYjmEwWBkVUzrpig
+RyuOcImE876hSSTqxgSYx0fBrdsY/Hw/SJFBUhoFTFxP5dv9o0pT+iiXSCvhC1O+
+KE+aOo6z0G1gdkEyaRGJX7NsHdyiUbbCFXPVZK1ssGPuEmXct/yUJ89JpdvA/Pa5
+G9npGJ2zTfoUES2FjzQEgRFFhmWZcjTKjLJZ8MsfUuZ9ZrhSNviusXnHA1mTSJik
+E/mI21bqUYRNxnImp6oqN+osUb9SmbB9R83S0Q+z72RIS1+772RNICFppoG8WMpg
+qySeFHWVJMl2/MVHBCz7oCTygv9EfLn/OPAO9v7Vd971WLSZSNQjCNA2rTUNDi1G
+LLXXQDDDFyV94pK8YG+Y299uL6xNXSznvujjiI2AuaqWF0M6XjWkOsPHeo0bWmXE
+QTqVpCVNCk08dwOXCLb3+u76tB70Zbg7ghkN5/u3pLyhyHiVVwyk1g9CjZmtwXsE
+Ji4Rt0WJnGSDy1M2nN/8ywOZjlEUkBvROaahobjuTNVpeUBFvBjt1zEcjyUn1kkq
+euOy5mobejpVot/5s/eWFAfERYstUZZFzLjIRfU66GSjlDcZunfrff7Cczg9MYHI
+cYVgWHPocWCK0HzwRbcmVjuTLJeUigAdK1o0nUawAoyiJImqnOSNh/lwv63L1fdA
+iZT2g1OE5vyXn3e9j0JdGASXctSUcdwCw5mVrFeTcZuJu8mOcr1ZIZSu24VJ2SM1
+ptjPVV5Dj5amDkoE00mqmyVmo4NSacOvF1IgtSMJ13YGSgp7OmE0B0NSeIo1YmY/
+CG3dEJoPvutdVzTtmOoFmYXwzKmEmzyPIQqOJ4F2gmJ22Iz3CYkp1JjkAZNwQucx
+ZJpTgaDHG/56//XzXqtm86Gyde1V4LBuJjVUUnKGeEHioOqEInaSbtOmFXpk8kVy
+dHykmknLEKaQ29nmHe8GoUe+A9udvw1mXDSvdfZ7OpaSSIckmmqB4uOgZJsoZbZj
+AkVzqS7KUR9Ci1Fhqp0xaN9K4NKzIATN+uT5/b8PzaF52tlaEKbNY4VOtKEJu0rC
+juJCMqYYbgySRZv1uBxGiUpRZLojvNk+djxzB4Lurvikzo9d7jEhVasZvB0rJmBU
+LkdOvYqiSjRMUE53I4hYNNF+inCfcaxomEuSVrApUR7s7Fw5+XMEyJzo7fMPvu7u
+q+LJpobCUkHgeQUkUEIbmGjUeCdbGRlAQrWCRhCSOysI9QEega0qfZvjzqJ1UnYf
+BV8fOK/vAZSP1yZR2U0dRga9GQCAm0pVT2uEu37u0mO6Bmk7OY5qoE3Cc16dbPWX
+8Vgy3dcq83UrwjDpuDg+dfZeQ9ChWyahiQn0VYfKUFis9kDhcIweXkXidYdKNqrk
+OtsARdRNo5c+veYtkR4cMTlakkDAVMd2cE5uB89CrmWCQjke5fcOlWIqXjWpAdWG
+zNmkyToQESvQ7qqN0GyhpyqbN72vRVYH3SesuEuOcel07qR2MOuse87fBB0yx05B
+7ahkqxKBk36rs4TFoRSoImGdyhVibqXvGw1DbLdNJpQhJPHOJ5PWuEZ920G53/5e
+hwDkBGv//k9OpNH+SymUTlo0itMgo8UIi5Q1tN2+5fvTONldwQ6jZEzQSKHwNTOB
+Sii4W2qL7yCGVucm1Ebhwn9fikqWYElcXK91EjWkqGBGrw/O5or7RQmuNYzFuJZA
+iyWObSwdYND2VbsZdrxoLJnfOFSW/ad+mK4BNzdSGOahYuMabDbKAyuLFOfCHF2x
+hYRGLYJNikpw2laEnGfbj0bJvOe0leEq6f+PhpzYt5FpNBoMmCzzWnJoYV7rdJ5U
+GZn6XG5DnVNBPMeEoIWRRvPOPxwWNN+oTCh8qI+xRIKSQf0OM8wQZ1jBWbl2jX6K
+A1r0zgnutT/roPNoc3judZrfUNxXbrLa3x8aFEoQpNwYp58hg/YNl8WpUZb2gy7K
+N+3N8+VO+9HyzJjcGe0UBoerAY82Eh2vHOd9IbFcV/F8aGqUFcjj8kjoQz2cP9/+
+2F4/+ub3NTuKEsZ1gMWIjjDKWtWpwzIkZIMeyt1ML8khaGtQ8r84mZX3zR16cMuT
+clECxrCqlhq9OnyL3AFWdNyuUi7s5aWRbojh/ARHB/+r9L/2rvwpbW2PM/oqoKXe
+Vqu1dbu1Yqsz9c3cX+57v0FIIMmwhDWAsiibBaHua1Wk6vW/fgnrOclJchKg7/5w
+z286I3787rvutocRsQuuO+cmjD/3rauLsvpIdH2kI++YlRnLknpriS2CctYsczWz
+8ZvtivAnbPeBcu+8hzLfjIqoDu41kOVT4l+kznCnswYqijvba5AIVCWTfuMoH6uO
+3jsADCbMcktTwzOMU1XHYW/t61a16GbAERwIw9uxW1fpZ95JkrItluHjXtWCcxHQ
+cn8ddPkk5xsIwzsGs2uagDRNFEsqxnDpREW9x1BOdy21REAiRcooSkDDxQizaTBP
+8u2vpy2QWPr/ooXowKfeWPLw0etApyIEoaS5LYMgqQsHiPJ8D0SZ7wlmSyw5kWn+
+K41WN+mtp8VaYDArEQ6Gi5UCRlD2THrztSLMLi17gjn6VQzaotGApitvZRh8TMjU
+5fJAem+MoDz8DoJ0VJtm/TEvE8ymtfRlxWyGePZqDzcwOSm/O/1QI4r+UIVRPsAo
+u4l5K8NNNL1cIMdpU3NfQIn8Y+oGeA6JZScO7nLcMfkb4MSBmjOtiTJHuJHNCDqS
+0G8tz1wwzAsY5Vhbfaw2uM3AaKIUgt+0BxXLe/U7yr0jGKXrAEY5sQGkuIi0ReVl
+ciiO7/K7Bd0oL3dgUrpuYTGwzyLLgURMe1JECNMh7RY0x8nXS5wBD/RYlaKkYGEd
+aUfA7+GfK2EMtICIeS7K8k+xG18hUg/pRnnqkKBsusiDnhhstsKihReSiQFdKPmn
+62Ix5m0ERFvL5Yo6A03q1oVACX53/l+9+hCYK0RIfJBMamufyxRKNyHRu9J6UjIC
+SL1BR+6HFb9VLzIvSxPVrA5iZmIp0iO2g4Rwxct4SEmZTfP9OEKhvANQthPJKelP
++jgaEyO9GyuTnRmCQilWj+gl5uF3FErQt7dRLsnyvxSDS0ov16R7PHdfKIjNlftK
+RRdK6ueOA4HyBxByts26DCWV9OBreLsyze629Nsd6s8QtTJykMITrSavTYYy59Fl
+i1heMEeZY0PBZc/JdJ6I8ieAfUYBZeBJD0q2Htt1Nq4TxmJ1qblsBeuPwDdmWk2V
+T/J5G3xT5K1sh0qZbJgw9qgLF0Iuoe/aN9C0DGawUfJJIVYLBA3XDKgDFEooTppR
+QBmu4aIkd++Jvt7dLQollGS0Ob4u5zimJaKd0UJ/KP1nLoQfh6K5to4btpd0PBt0
+94nyGwrlJWjq2/ZS5nvcmPaSLVFEny90LkMpRG6QeVJCSRzvYgUaBuI0mRuXozyQ
+qFTbQy7LKBIo47DcWGIrQSl142I5C87Q2w1JaeSmXSRsZz/hYaDMn8LKU51vJZEr
+cpSBhrZk0lmqf5R7MpRCPg5HIGuWXhNFOogc1+I5zW71D1IeuDl2DuEMPf+x3XFe
+RPiEm4gGSk8qPAyUrqM9OHy3vwaKWTJrFOM1c/KhoPwWgsQy322graN+XrX5Q/ON
+BDEclLcUXDia6MwQTqHUwL3PK7d/vNkwMRyUgiE6gL7u1olW36M+YIvLKU1SkskQ
+MSSU+RNJPbOd6JrMo3+iPiCcyiXheRaSdnpZluVp/pgYFsqdy1YLvysB/7aakIWi
+zruOFoHgiK7lkvFI+rmwneacN4NCCUMSVfzHKZRj2Lt9KesS8hPC5eR1TzT/SlDu
+xL2/mXiTT4EBofwpq2VJGA4MaSF8ZFMyM8UK2emUAIYnEWePh4Qyf3oJy8Db7ryB
+uVlYRwR/lacSx7RtOEA9d8Wz7x4Oyp0HmOGOj71+jwUtmESBq8RYOUriOXLlHwpK
+19EhzPA80C5VEEzBTz4lcgiU7q2t4dDSdfsAfSMPzQ4qCCbhDlPBFCnjeMhNDEcu
+qyewSXfMjytPh8IGiZf0GbeTjcp1kBoCStfRAxy7V0egoUab8ge1VhL45tRX6OY6
+SBT/atTYWjZBDR7lmUR3JuBh0YUXyol9mhccJdPc7LiPOGOEPxQKF+ssl/X1jxMW
+w+qpJKWch+eeUDFmN3Df94rUzAUDhTLNt6c3QqUofxUKFAr92Xeo5OY6uoDFNC+d
+blxSS0fTNbH7WCvHhey2W8wIl7YSjd1IOTEwlI5buIZgH5Nufqx8UavmBGM1p8fj
+ETgfKYCyEOG98b4SixMQ5c6BJPZYs6CmL5VhUsFsoyzOl7HpYC9qCyeKW76+7Duo
+La5ziVTaZ5WHghUbhqHnlJOkvZlGelDRJUFcACirZxJ3iRgNVtOfrlHa57wM4+Gv
+B4cSAHV0DoO0oyaDp15of6g7eJMSN2CoAYGkAFdTPZeQchK1mjL3GeuDwxUnk+su
+zxX6y3/AHtnOdwkpR5Az1lN4nxxMedoDtm5fOh5N+/pACXafqnkMUqo7c6juwUWu
+gwkfFYrFeZpxZtLbhe17Y34oJCtfAi5cYeFjCZNNx1E2HuFi+60RD5KPRCK76HFR
+zTLRuSLKsZd4ezMqTM82rsosXPyIJw2w/nBHmZRWzUUKTXKG3IEruPZBMjX9ofFD
+VQmlyh7S+Ccdv+FZWuNk4rFgOOA26nqgMGPid6zdOAy2x2lZ9SheKye3KR3mUkks
+19SWn83r+CivUfVikmE4/CxY3keRZ+F9KRCxrdRg01HYlE7pdBk+orFeiqtAPuXO
+lRd73OAnWnny81o3Vyw2vF+gsnRPJhNhzNVHNCnHNnQsE6truMppDW88mjsOayvR
+HdrzYC3kL/zRL0px3ZiNxkoljaToEC2Wm1ir2TghnPqZheaip6BckVRarVF5grSW
+2rvE+KIZqGPMydAevpYN6LND2CcDsEQzgdee9iiPvl1+R6C0j2BfiVjBsZpbcZyp
+IyYTxIjTe0ZoE/8wiHnhK4Z7i2GMcJHK+68ok57f1HVkZRXDoQeutKdeI2m/nkhj
+Wt8JLfPy+0GIJldSoiSFCIBd869M+p51CgPmc8ajSk5vWlleLuSkfKv/tptlCcNs
+bjdYRm0lSFG/KXnTLP/2jZE7kjjUDBxfeVXueSkWQfy3ckoaASkwffkLThq4r6jq
+fJrCqmI1KTn9ymTsWVdxXLriIphH+WjSpcwKrRk/kGdewDDv6HkzkvEo1wx/SIMh
+uz47KfNC2s7Sh/BBJB1P1RU3fu4OJPrdxzW3tk+3aal6AtGfzmQLAUVz7j6VCGV/
+B/xa2e/Un1ooEdMSakejTyR93Ok3A7goal5Y1BdpkqzaFS/qEbaUgzgsicH1oizA
+yPrxQU7ODuyE7PiUSox0z0uHw1UK2u4TCKR9+o1pcM+8YlN0RDG4vMHdqOQQ/gtI
+JicHfSvasqrQxAiVu8NmpIevF9USnb3bKjhY+fGVaeBvDs12f2cEhXFyqaxq0nj5
+Dcxn1zaspiE866jtC7KC3fQ9fPnGp3rg5+4EcIv2t7PDOrRutqysy+kZEldhaW9S
+ozq0d9ATyZn5YR4FF3COLn2WECzQ2PUybE69QHD32GvmTAz/ELzZNLf84T3cpCwU
+cyXVdhp1eNsl5Njmxq84qm82jy8sLWJEyL0Q6OLI0QI5Nj370mr6Rc9snltd/4xZ
+Q/Q/fsu3jnxMv375q/8thXV02YZB0buHM9FG2semf+3/eQCAzq0ISNXSDv/D2Y5r
+ZnJ65N2r/9+/ITGLvF9ZXv+0+PULgv8/Hv8zOb35+t0ri+lv8MzW8dGV1amldZvt
+04fFr398Xvzwyba+NPXfjZe/Waymv9kzWy3j43PCGx+3WKxm0z/vn2f0/Q8AEmK8
+DepDQQAAAA5lWElmTU0AKgAAAAgAAAAAAAAA0lOTAAAAAElFTkSuQmCC">
+	<style>
+		/*shared*/
+		:root {
+			--box-size: 600px;
+		}
+		::selection {
+		  background: #cb1c09;
+		}
+		html, body {
+			height: 100vh;
+			margin: 0;
+			background: rgb(23, 3, 4);
+			color: rgb(180, 180, 180);
+		}
+		#gradient {
+			width: 100vw;
+			height: 100vh;
+			position: fixed;
+			top: 0;
+			left: 0;
+			z-index: 0;
+			background: linear-gradient(180deg, rgb(10, 10, 10) 0%, rgba(37,7,9,1) 70%);
+		}
+
+		#splash-image {
+			box-shadow: 0 0 400px 50px #C94006;
+			width: 1235px;
+			height: 693px;
+			background-color: #cb1c09;
+			background-image: url("fire.jpg");
+			background-repeat: no-repeat;
+			background-size: 1235px, 702px;
+			position: absolute;
+			left: 50%;
+			transform: translateX(-50%);
+			top: 150px;
+			border-radius: 20px;
+		}
+		h1 {
+			/*text-align: center;*/
+			padding: 225px 0;
+			padding-left: 265px;
+			font-size: 4em;
+			color: rgb(255, 0, 0);
+		}
+
+
+		#thing-container {
+			width: 90vw;
+			top: 870px;
+			position: absolute;
+			left: 50%;
+			transform: translateX(-50%);
+		}
+
+		.message-container {
+			overflow: hidden;
+		    padding-top: 2%;
+		    padding-left: 3%;
+		    padding-right: 3%;
+		    padding-bottom: 5%;
+		}
+		.text, .image {
+			background-color: rgb(30, 30, 30);
+		    border: 3px solid #f50d0e;
+		    border-radius: 40px;
+		    padding: 2%;
+		    width: var(--box-size);
+		    height: var(--box-size);
+			word-wrap: break-word;
+		}
+		.left-text {
+			float: left;
+		    /*margin-left: 50px;*/
+		}
+		.right-text {
+			float: right;
+		    /*margin-right: 50px;*/
+		}
+		h2 {
+			text-align: center;
+			font-size: 3em;
+		}
+		p {
+			font-size: 1.7em;
+		}
+
+
+		/*.image {
+			width: var(--box-size);
+			height: var(--box-size);
+			padding: 0;
+		}*/
+		.image img {
+			width: 100%;
+			border-radius: 30px;
+		}
+		.left-image {
+			float: left;
+		}
+		.right-image {
+			float: right;
+		}
+
+		/*landscape*/
+		@media (orientation:landscape) {
+
+		}
+
+		/*portrait*/
+		@media (orientation:portrait) {
+			h1 {
+				margin-top: -100px;
+				margin-left: 0px;
+				font-size: 6em;
+			}
+			#splash-image {
+				top: 300px;
+				width: 100vw;
+				background-size: 100vw, auto;
+			}
+			#thing-container {
+				top: 1000px;
+			}
+			.waveAnimation { display: none; }
+
+			.left-text, .right-image, .right-text, .left-image {
+				float: none !important;
+				margin: 50px auto;
+				display: block;
+			}
+			p {
+				padding: 0 20px;
+			}
+		}
+		h1 {
+		  background: linear-gradient(70deg, #f00, #111, #f00);
+		  background-size: 80% 100%;
+		  background-repeat: repeat;
+		  background-clip: text;
+		  -webkit-background-clip: text;
+		  -webkit-text-fill-color: transparent;
+		  animation: shineText 8s linear infinite;
+		}
+		@keyframes shineText {
+		  0%{
+		    background-position-x: -400%;
+		  }
+		  100%{
+		    background-position-x: 400%;
+		  }
+		}
+
+
+		/*wave animation*/
+		.waveAnimation {
+			bottom: -51vh;
+			width: 100vw;
+			height: 100vh;
+			position: fixed;
+			pointer-events: none;
+		}
+		.w1 {
+		    stroke: none;
+		    /*fill: #b3696c;*/
+		    fill: rgb(255, 232, 8);
+		    opacity: 1%;
+		    animation: move1 5s ease-in-out infinite;
+		}
+		.w2 {
+		    stroke: none;
+		    /*fill: #b3696c;*/
+		    fill: rgb(255, 154, 0);
+		    opacity: 2%;
+		    transform: translate3d(0, 0, 0);
+		    animation: move2 4s ease-in-out infinite;
+		}
+		.w3 {
+		    stroke: none;
+		    /*fill: #cf2127;*/
+		    fill: rgb(127, 0, 0);
+		    opacity: 5%;
+		    transform: translate3d(0, 0, 0);
+		    animation: move3 6s ease-in-out infinite;
+		}
+		@keyframes move1 {
+		    0% {
+		    	transform: translateX(-500px) scaleX(2.5);
+		    }
+		    25% {
+		    	transform: translateX(-100) scaleX(2.5);
+		    }
+		    50% {
+		    	transform: translateX(0) scaleX(2.5);
+		    }
+		    75% {
+		    	transform: translateX(-100) scaleX(2.5);
+		    }
+		    100% {
+		    	transform: translateX(-500px) scaleX(2.5);
+		    }
+		}
+		@keyframes move2 {
+		    0% {
+		    	transform: translateX(-600px) scaleX(3);
+		    }
+		    25% {
+		    	transform: translateX(-100) scaleX(2.5);
+		    }
+		    50% {
+		    	transform: translateX(0) scaleX(3);
+		    }
+		    75% {
+		    	transform: translateX(-100) scaleX(2.5);
+		    }
+		    100% {
+		    	transform: translateX(-600px) scaleX(3);
+		    }
+		}
+		@keyframes move3 {
+		    0% {
+		    	transform: translateX(-800px) scaleX(3);
+		    }
+		    25% {
+		    	transform: translateX(-100) scaleX(2.5);
+		    }
+		    50% {
+		    	transform: translateX(0) scaleX(3);
+		    }
+		    75% {
+		    	transform: translateX(-100) scaleX(2.5);
+		    }
+		    100% {
+		    	transform: translateX(-800px) scaleX(3);
+		    }
+		}
+
+		#smog-fade {
+			position: fixed;
+			width: 100vw;
+			height: 100vh;
+			top: 0;
+			left: 0;
+			background: #ccc;
+			background: radial-gradient(rgba(100,100,100,1), rgba(100,100,100,0.5) 80%, rgba(100,100,100,0.1));
+			z-index: 999;
+			opacity: 0;
+			pointer-events: none;
+			backdrop-filter: blur(4px);
+			/*animation: smog 6s ease-in-out 1;*/
+		}
+		@keyframes smog {
+			0% {
+				opacity: 0;
+			}
+
+			50% {
+				opacity: 1;
+			}
+
+			0% {
+				opacity: 0;
+			}
+		}
+		</style>
+	</head>
+	<body>
+		<div id="gradient"></div>
+		<?php include "header.html"; ?>
+		<main>
+			<div id="splash-image"><h1>Biggest Threats</h1></div>
+			<div id="thing-container">
+				<div id="1" class="message-container">
+					<div class="left-text text">
+						<h2>Deforestation</h2>
+						<p>because trees store carbon as they grow, cutting or burning down trees releases that carbon into the atmosphere. Farmers may cut down trees or clear land using fire to produce soya for animal feed, such as in the Amazon. In other parts of the world, natural forests are cleared for timber, mining or palm oil.</p>
+					</div><div class="right-image image">
+						<img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/08/13/14/amazon-deforestation.jpg?width=1200&height=1200&fit=crop">
+					</div>
+				</div>
+				<div id="2" class="message-container">
+					<div class="right-text text">
+						<h2>Plastic and waste</h2>
+						<p>plastics are made from fossil fuels, releasing emissions through their production. Globally, about 40% of plastics are used as packaging. Because so little is recycled (and it would be hard to recycle that much plastic anyway), dealing with waste releases emissions when incinerated (burned) or put into landfill – making it a bigger climate problem than it initially seems.</p>
+					</div><div class="left-image image">
+						<img src="https://www.reuters.com/resizer/rQfUftEC2p_rSel69igZ0XXBkRw=/1920x1920/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/52MLDFVC4NJSHM5WBTKVBG6FKU.jpg">
+					</div>
+				</div>
+				<div id="3" class="message-container">
+					<div class="left-text text">
+						<h2>Food production</h2>
+						<p>livestock reared for meat and dairy products emit methane, and agricultural soils emit gases like nitrous oxide, which is made from nitrogen in the soil through the use of fertiliser. As food production increases (with more fertilisers, more livestock, and the need for more crops to feed livestock), emissions will also increase.</p>
+					</div><div class="right-image image">
+						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe3LTO2xCqjJLU7JHfmLjgx0UXlN4LM60zxw&usqp=CAU">
+					</div>
+				</div>
+				<div id="4" class="message-container">
+					<div class="right-text text">
+						<h2>Transport</h2>
+						<p>cars, buses, trains, trucks, ships and planes, (unless electric and charged with renewable energy), all produce emissions by burning fossil fuels. In the UK, transport is the biggest contributor to climate change, responsible for 27% of emissions in 2019, mostly from cars. International aviation and shipping will continue to be a significant contributor to climate change until demand reduces or alternatives to fossil fuels become available.</p>
+					</div><div class="left-image image">
+						<img src="https://i.guim.co.uk/img/media/0a400b74e66445652fd78d4b773a266e02d835e4/0_308_4773_2864/master/4773.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=e60884cca584595fcecba15d3ee9295b">
+					</div>
+				</div>
+				<div id="5" class="message-container">
+					<div class="left-text text">
+						<h2>Energy production</h2>
+						<p>a lot of power generation for electricity and the vast majority of home heating are still done by burning fossil fuels, such as gas. In the UK, emissions from electricity have gone down rapidly in recent years, thanks to our reductions in burning coal for energy and dramatic increases in renewable energy generation.</p>
+					</div><div class="right-image image">
+						<img src="https://seas.harvard.edu/sites/default/files/styles/embedded_image_large/public/image_114.jpg?itok=iajlbM8y">
+					</div>
+				</div>
+			</div>
+			<div class="waveAnimation">
+		      <svg viewBox="0 0 500 150" preserveAspectRatio="none">
+		        <path
+		          class="w1 waveTop" d="M-8.74,71.55 C289.78,255.11 349.60,4.47 505.36,34.05 L500.00,150.00 L0.00,150.00 Z"/>
+		        <path
+		          class="w2 waveMiddle" d="M-23.42,125.83 C187.63,45.89 299.38,57.73 526.80,123.86 L500.00,150.00 L0.00,150.00 Z"/>
+		        <path
+		          class="w3 waveBottom" d="M-23.42,125.83 C172.96,-152.44 217.55,183.06 504.22,55.77 L500.00,150.00 L0.00,150.00 Z"/>
+		      </svg>
+		    </div>
+		    <div id="smog-fade" style=""></div>
+		</main>
+		<script>
+			//force #splash-image to behave properly on mobile
+			function AdjustSplash() {
+				if(window.innerHeight > window.innerWidth){
+				    let splashWidth = screen.width;
+				    let splashHeight = splashWidth * (16/11.3);
+				    document.getElementById("splash-image").style.height = splashHeight + "px";
+				}
+				else
+					document.getElementById("splash-image").style.height = "693px";
+			}
+			AdjustSplash();
+			//call if window resized
+			$(window).resize(function() {
+				AdjustSplash();
+			});
+
+
+			//toggle smog effect
+			var toggled = false;
+			$(window).scroll(function() {
+			   var hT = $('#4').offset().top,
+			       hH = $('#4').outerHeight(),
+			       wH = $(window).height(),
+			       wS = $(this).scrollTop();
+			    console.log((hT-wH) , wS);
+			   if (wS > (hT+hH-wH) && !toggled){
+			    	document.getElementsByTagName("body")[0].style.overflow = "hidden";
+			    	document.getElementById("smog-fade").style.animation = "smog 8s cubic-bezier(0,0.7,0.8,1) 1";
+			    	setTimeout(() => document.getElementsByTagName("body")[0].style.overflow = "visible", 6000);
+			    	toggled = !toggled;
+			   }
+			});
+		</script>
+	</body>
+</html>
